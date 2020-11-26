d4004dfdea9c94355f59add736bc46b2e58ca201
Olivia <admin@amselhof.ch>
2020-11-26 16:56:35 +0100

    add FadeIn on Scroll / color change on Scroll

--- main.js
+++ main.js
@@ -1,3 +1,45 @@
+
+// click function that shows scroll Y position
+const sections = document.querySelectorAll('section');
+const homeSection = document.getElementById('home'); //console.log(homeSection);
+const navbarBrand = document.querySelector('.navbar-brand');
+
+function getPosition(element) {
+  let yPosition = 0;
+
+  while(element) {
+      yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
+      element = element.offsetParent;
+  }
+
+  return yPosition;
+}
+
+//fade In Elements on Scroll
+for(let i = 0; i < sections.length; i++){
+    const section = sections[i];
+    const sectionTop = getPosition(section); console.log(sectionTop);
+    if(section === homeSection){
+      section.style.opacity = 1;
+    }else{
+      section.style.opacity = 0;
+    }
+    
+    window.addEventListener("scroll", () => {
+      //change color of navBrand on Scroll
+      if(window.scrollY > 0){
+        navbarBrand.style.color = '#3513f0';
+      }else{
+        navbarBrand.style.color = 'rgba(0,0,0,.9)';
+      }
+      
+      //fadeIn Elements on Scroll
+      if(window.scrollY + 200 >= sectionTop){
+        section.style.opacity = 1;
+      }
+    })
+}
+
 //after page load...
 jQuery(document).ready(function() {
 
